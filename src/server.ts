2b3cb6079b2ebb2b50ac4884ab744e2dc7c87425
Linnea Nilsson <linnea.nilsson97@hotmail.com>
2025-09-24 13:38:26 +0200

    - Laggt till gitignore-fil. - La till middleware.

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,24 @@
-console.log("hello world")
+import express from "express"
+import type {Express, RequestHandler} from "express"
+
+const port = process.env.PORT
+const app: Express = express()
+
+const logger: RequestHandler = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`)
+    next()
+}
+
+app.use("/", logger)
+app.use(express.json())
+app.use("/products", productsRouter)
+app.use("/users", usersRouter)
+app.use("/cart", cartRouter)
+
+// TODO: lägg till router mapp
+// TODO: lägg till data mapp
+// TODO: sätt upp dynamoDB
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}...`)
+})
