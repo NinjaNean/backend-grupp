f7579d7abca7fb3e08ece2bd032067d5cb3dcc72
Linnéa Nilsson <linnea.nilsson97@hotmail.com>
2025-09-25 11:05:55 +0200

    - GET endpoints för att få alla produkter.

--- src/router/products.ts
+++ src/router/products.ts
@@ -1,6 +1,57 @@
 import express from "express";
-import type { Request, Response, Router } from "express";
+import type { Response, Router } from "express";
+import { db, myTable } from "../data/db.js";
+import { QueryCommand } from "@aws-sdk/lib-dynamodb";
 
 const router: Router = express.Router();
+
+type Product = {
+  image: string;
+  amountStock: number;
+  sk: string;
+  pk: string;
+  price: number;
+  name: string;
+};
+
+type SuccessResponse = {
+  success: boolean;
+  count: number;
+  items: Product[];
+};
+
+type ErrorResponse = {
+  success: boolean;
+  message: string;
+  error: string;
+};
+
+type GetResult = Record<string, any> | undefined;
+router.get("/", async (req, res: Response<SuccessResponse | ErrorResponse>) => {
+  try {
+    const result: GetResult = await db.send(
+      new QueryCommand({
+        TableName: myTable,
+        KeyConditionExpression: "pk = :products AND begins_with(sk, :productId)",
+        ExpressionAttributeValues: {
+          ":products": "products",
+          ":productId": "productId#",
+        },
+      })
+    );
+
+    res.status(200).json({
+      success: true,
+      count: result.Count ?? 0,
+      items: result.Items ?? [],
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Could not fetch products",
+      error: (error as Error).message,
+    });
+  }
+});
 
 export default router;
