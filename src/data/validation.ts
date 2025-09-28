6d17d792e163b01df8e4ffcd667cddbac420ef8f
Akevonwolfe <94.andreas.svensson@gmail.com>
2025-09-28 19:50:51 +0200

    CRUDfor /users ENDPOINT and simple Zod for PUT and POST

--- src/data/validation.ts
+++ src/data/validation.ts
@@ -1,1 +1,10 @@
 import * as z from "zod";
+
+
+const userSchema = z.object({
+    pk: z.string().min(1).regex(/^user\w+/), // pk must start with "user" followed by one or more word characters
+    sk: z.string().min(1).regex(/^meta$/),   // sk must be exactly "meta"
+    name: z.string().min(1)                  // name must be a non-empty string
+    })
+
+export  { userSchema }
