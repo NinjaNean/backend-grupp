168e1630d3c9e1dc5aa766ce47daf73663af39b2
Linnéa Nilsson <linnea.nilsson97@hotmail.com>
2025-09-30 13:35:05 +0200

    - Lagt till två zod schema för produkt och ändrad produkt. - Validerat body på post och put.

--- src/data/validation.ts
+++ src/data/validation.ts
@@ -1,10 +1,31 @@
 import * as z from "zod";
 
-
 const userSchema = z.object({
-    pk: z.string().min(1).regex(/^user\w+/), // pk must start with "user" followed by one or more word characters
-    sk: z.string().min(1).regex(/^meta$/),   // sk must be exactly "meta"
-    name: z.string().min(1)                  // name must be a non-empty string
-    })
+  pk: z
+    .string()
+    .min(1)
+    .regex(/^user\w+/), // pk must start with "user" followed by one or more word characters
+  sk: z
+    .string()
+    .min(1)
+    .regex(/^meta$/), // sk must be exactly "meta"
+  name: z.string().min(1), // name must be a non-empty string
+});
 
-export  { userSchema }
+const ProductSchema = z.object({
+  pk: z.literal("products"),
+  sk: z.string().regex(/^productId#\d+$/),
+  image: z.string(),
+  amountStock: z.number(),
+  price: z.number(),
+  name: z.string().min(1),
+});
+
+const NewProductSchema = z.object({
+  image: z.string(),
+  amountStock: z.number(),
+  price: z.number(),
+  name: z.string().min(1),
+});
+
+export { userSchema, ProductSchema, NewProductSchema };
--- src/router/products.ts
+++ src/router/products.ts
@@ -4,18 +4,23 @@ import { db, myTable } from "../data/db.js";
 import { DeleteCommand, GetCommand, PutCommand, QueryCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
 import type { DeleteCommandOutput, PutCommandOutput } from "@aws-sdk/lib-dynamodb";
 import type { ErrorResponse, GetResult, SuccessResponse } from "../data/types.js";
+import { ProductSchema, UpdateProductSchema } from "../data/validation.js";
 
 const router: Router = express.Router();
+
+type ProductParam = {
+  productId: string;
+};
 
 router.get("/", async (req, res: Response<SuccessResponse | ErrorResponse>) => {
   try {
     const result: GetResult = await db.send(
       new QueryCommand({
         TableName: myTable,
-        KeyConditionExpression: "pk = :products AND begins_with(sk, :productId)",
+        KeyConditionExpression: "pk = :p AND begins_with(sk, :pId)",
         ExpressionAttributeValues: {
-          ":products": "products",
-          ":productId": "productId",
+          ":p": "products",
+          ":pId": "productId#",
         },
       })
     );
@@ -34,10 +39,7 @@ router.get("/", async (req, res: Response<SuccessResponse | ErrorResponse>) => {
   }
 });
 
-type ProductParam = {
-  productId: string;
-};
-
+// TODO: lägg till felmeddelanden osv
 router.get("/:productId", async (req: Request<ProductParam>, res) => {
   const productId: string = req.params.productId;
 
@@ -54,7 +56,7 @@ router.get("/:productId", async (req: Request<ProductParam>, res) => {
   res.status(200).send(result.Item);
 });
 
-// TODO byt ut object mot något annat
+// TODO: byt ut object mot något annat
 router.delete("/:productId", async (req: Request<ProductParam>, res: Response<object | ErrorResponse>) => {
   const productId: string = req.params.productId;
 
@@ -70,7 +72,6 @@ router.delete("/:productId", async (req: Request<ProductParam>, res: Response<ob
         ReturnValues: "ALL_OLD",
       })
     );
-    console.log(result);
 
     res.status(200).send({
       message: "The product has been removed.",
@@ -78,13 +79,13 @@ router.delete("/:productId", async (req: Request<ProductParam>, res: Response<ob
     });
   } catch (error) {
     if ((error as Error).name === "ConditionalCheckFailedException") {
-      res.status(404).json({
+      res.status(404).send({
         success: false,
-        message: `${productId} dose not exist.`,
+        message: `${productId} does not exist`,
         error: (error as Error).message,
       });
     } else {
-      res.status(500).json({
+      res.status(500).send({
         success: false,
         message: "Could not delete product.",
         error: (error as Error).message,
@@ -93,8 +94,21 @@ router.delete("/:productId", async (req: Request<ProductParam>, res: Response<ob
   }
 });
 
+// TODO: lägg till felmeddelanden osv
 router.post("/", async (req, res) => {
-  const newProduct = req.body;
+  const parsed = ProductSchema.safeParse(req.body);
+
+  if (!parsed.success) {
+    res.status(400).send({
+      success: parsed.success,
+      message: "The product information is invalid.",
+      error: parsed.error,
+    });
+
+    return;
+  }
+
+  const newProduct = parsed.data;
 
   let result: PutCommandOutput = await db.send(
     new PutCommand({
@@ -103,38 +117,59 @@ router.post("/", async (req, res) => {
     })
   );
 
-  res.status(202).json({
-    message: "Produkten är tillagd.",
+  res.status(201).send({
+    message: "The product has been added.",
   });
 });
 
-type NewProduct = {
-  image: string;
-  amountStock: number;
-  price: number;
-  name: string;
-};
-
-router.put("/:productId", (req: Request<ProductParam>, res) => {
+// TODO: byt ut object mot något annat
+router.put("/:productId", async (req: Request<ProductParam>, res: Response<object | ErrorResponse>) => {
   const productId: string = req.params.productId;
-  const newProduct = req.body;
 
-  let result = db.send(
-    new UpdateCommand({
-      TableName: myTable,
-      Key: {
-        pk: "products",
-        sk: productId,
-      },
-      UpdateExpression: "SET image = :i, amountStock = :a, price = :p, name = :n",
-      ExpressionAttributeValues: {
-        ":i": newProduct.image,
-        ":a": newProduct.amountStock,
-        ":p": newProduct.price,
-        ":n": newProduct.name,
-      },
-    })
-  );
+  try {
+    const parsed = UpdateProductSchema.safeParse(req.body);
+
+    if (!parsed.success) {
+      res.status(400).send({
+        success: parsed.success,
+        message: "The product information is invalid.",
+        error: parsed.error,
+      });
+
+      return;
+    }
+
+    const updatedProduct = parsed.data;
+
+    let result = await db.send(
+      new UpdateCommand({
+        TableName: myTable,
+        Key: {
+          pk: "products",
+          sk: productId,
+        },
+        UpdateExpression: "SET image = :i, amountStock = :a, price = :p, name = :n",
+        ExpressionAttributeValues: {
+          ":i": updatedProduct.image,
+          ":a": updatedProduct.amountStock,
+          ":p": updatedProduct.price,
+          ":n": updatedProduct.name,
+        },
+        ReturnValues: "ALL_NEW",
+      })
+    );
+
+    res.status(200).send({
+      message: "Product updated.",
+      Item: result.Attributes,
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Could not edit product.",
+      error: (error as Error).message,
+    });
+  }
 });
 
 export default router;
