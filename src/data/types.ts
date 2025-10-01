a307c24f4b6ea174b7b75ac110445789e04ea531
AkeVonWolfe <94.andreas.svensson@gmail.com>
2025-10-01 14:12:45 +0200

    merge save

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,16 +1,49 @@
+type Product = {
+  image: string;
+  amountStock: number;
+  sk: string;
+  pk: string;
+  price: number;
+  name: string;
+};
 
-// user types
+export interface user {
+  pk: string;
+  sk: string;
+  name: string;
+}
+
+export type CartItem = {
+  id: string;
+  userId: string;
+  productId: string;
+  amount: number;
+};
+
+export type SuccessResponse = {
+  success: boolean;
+  count: number;
+  items: Product[] | user[] | CartItem[];
+};
+
+export type ErrorResponse = {
+  success: boolean;
+  message: string;
+  error: string;
+};
+
+export type GetResult = Record<string, any> | undefined;
+
+
+
+
+// user types here is user response and request types - Start here 
 export interface User {
     pk: string
     sk: string
     name: string
 }
 
-export type ErrorResponse = {
-    success: false
-    message: string
-    error: string
-}
 
 // user response types
 export type GetUsersResponse = {
