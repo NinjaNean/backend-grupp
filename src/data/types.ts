b95961e65a4c0c0ac407ef8ea6cb648008812670
Mikaela <mikaela.andersson@student.nbi-handelsakademin.se>
2025-10-01 13:53:52 +0200

    Revert "Merge branch 'dev' into cart-validation"
    This reverts commit 02ebb8591fc861ac80448c716b1f33ced072c9ce, reversing
    changes made to 4acbe0b813d58003b0cf44c88c044fc551d8f324.

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,35 +1,3 @@
-type Product = {
-  image: string;
-  amountStock: number;
-  sk: string;
-  pk: string;
-  price: number;
-  name: string;
+type TEMP = {
+  temp: string;
 };
-
-export interface user {
-  pk: string;
-  sk: string;
-  name: string;
-}
-
-export type CartItem = {
-  id: string;
-  userId: string;
-  productId: string;
-  amount: number;
-};
-
-export type SuccessResponse = {
-  success: boolean;
-  count: number;
-  items: Product[] | user[] | CartItem[];
-};
-
-export type ErrorResponse = {
-  success: boolean;
-  message: string;
-  error: string;
-};
-
-export type GetResult = Record<string, any> | undefined;
